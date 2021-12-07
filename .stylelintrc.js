13257a904c31535b9855322b9705a51776b48278
zhouyuanli <zhouyuanli@zuoyebang.com>
2021-12-07 16:25:10 +0800

    first commit

---
+++ .stylelintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: "stylelint-config-standard",
+  // "plugins": [
+  //   "stylelint-less"
+  // ],
+  // "customSyntax": "postcss-less",
+  rules: {
+    "block-no-empty": null,
+    "at-rule-no-unknown": null,
+    'selector-pseudo-class-no-unknown': null,
+    'selector-class-pattern': null,
+    'no-descending-specificity': null
+  }
+}
