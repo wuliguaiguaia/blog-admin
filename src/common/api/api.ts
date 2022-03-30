2182ba4c15c7d89182d8febcdefb1dea9c96ead0
zhouyuanli <zhouyuanli@zuoyebang.com>
2022-03-30 21:59:00 +0800

    fix: webhook 转发

--- src/common/api/api.ts
+++ src/common/api/api.ts
@@ -19,7 +19,7 @@ export const post = {
   adduser: `${apiPrefix}/user`,
   login: `${apiPrefix}/login`,
   register: `${apiPrefix}/register`,
-  webhook: '/webhook/manual',
+  webhook: '/api/webhook/manual',
 }
 
 export const remove = {
