9ae8a7c70fd7ccf69e2df8c476d330a3eb75300c
zhouyuanli <zhouyuanli@zuoyebang.com>
2022-05-15 17:46:38 +0800

    test

--- src/pages/management/pages/workBench/index.tsx
+++ src/pages/management/pages/workBench/index.tsx
@@ -49,7 +49,7 @@ const Workbench: FunctionComponent = () => {
   return (
     <div className={styles.wrapper}>
       <div>
-        跳转至
+        跳转至 ohhh
         <a href="https://orangesolo.cn" target="_blank" rel="noreferrer">前台</a>
       </div>
       <div>
